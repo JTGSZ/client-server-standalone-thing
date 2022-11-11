a6e987139d96b3bfb91cb1f5959d8d919c814a04
JTGSZ <46565256+JTGSZ@users.noreply.github.com>
2022-11-11 03:12:22 -0500

    dashes and healthbars

--- Godot4/client/04-PlayerBodies/PlayerTemplate.gd
+++ Godot4/client/04-PlayerBodies/PlayerTemplate.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
+class_name PlayerTemplate
+
 #the direction we are moving
 #var velocity: Vector2 = Vector2()
 #the direction we are facing.
-var facing_direction: Vector2 = Vector2()
+var facing_direction: Vector2 = Vector2(-1, 0)
 
 #testcase for health
 var health = 5000
@@ -18,7 +20,24 @@ var spell = preload("res://99-TestCases/Spell.tscn")
 var can_fire = true
 var rate_of_fire = 0.4
 #Test-----------------------------------------
+#floating text effect for damage/heals or some shit.
 var floating_text = preload("res://06-Unsorted/FloatingText.tscn")
+#are we currently dashing?
+var currently_dashing = false
+var dash_vector: Vector2 = Vector2()
+var dash_ticks_left = 0
+
+var dash_ticks = 25
+var dash_speed = 150
+
+#Character States
+enum {
+	NORMAL, #Business as usual
+	STUNNED #Stops the body from accepting any inputs
+}
+var character_state = NORMAL
+
+var dash_duration = 0.2
 
 #onready vars basically get set in _ready lol
 #this is just animation player and state shit
@@ -36,30 +55,40 @@ func _ready():
 func _physics_process(_delta):
 	velocity = Vector2()
 	
-	if body_player_controlled:
+	#If we r controlled, not currently dashing, and our character state is normal
+	if body_player_controlled && !currently_dashing && character_state == NORMAL:
 		read_movement_inputs()
 
+	#If currently dashing set our velocity to the dash vector
+	if currently_dashing:
+		velocity = dash_vector
+		dash_ticks_left -= 1
+		if dash_ticks_left <= 0:
+			currently_dashing = false
+			speed -= dash_speed
+		
+	#Animation shit lol
 	if velocity != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", facing_direction)
 		animationTree.set("parameters/Run/blend_position", facing_direction)
-#		animationTree.set("parameters/Attack/blend_position", facing_direction)
 		animationState.travel("Run")
 	else:
 		animationTree.set("parameters/Idle/blend_position", facing_direction)
 		animationState.travel("Idle")
 	animationTree.set("parameters/Attack/blend_position", facing_direction)
-	if body_player_controlled:
-		read_attack_inputs()
+	
+	if body_player_controlled && character_state != STUNNED:
+#		read_attack_inputs()
+		pass
 	
 	#This basically makes sure our diagonals don't go xtra fast
 #	velocity = velocity.normalized()
 	set_velocity(velocity * speed)
 	move_and_slide()
-#	velocity = velocity
 
 func read_movement_inputs():
 	if ActorController.inputs_enabled:
-
+		
 		# If we hit ctrl, we just swap directions.
 		#Even if something is targeted you can force a direction change, and let go to snap back.
 		#Something to consider is if we also add the velocities here, so u can strafe etc.
@@ -86,13 +115,24 @@ func read_movement_inputs():
 				velocity.x += 1
 				facing_direction = Vector2(1, 0)
 
-		
 			if ActorController.currently_selected:
 				var target_pos = ActorController.currently_selected.get_global_position()
 				var our_pos = get_global_position()
 				
 				facing_direction = our_pos.direction_to(target_pos)
+				
+		if Input.is_action_just_pressed("spacebar"):
+			if velocity:
+				dash_vector = velocity
+			else:
+				dash_vector = facing_direction
+				
+			currently_dashing = true
+			dash_ticks_left = dash_ticks
+			speed += dash_speed
 
+
+#Read attack input lol
 func read_attack_inputs():
 	if ActorController.inputs_enabled:
 		if Input.is_action_just_pressed("spacebar") and can_fire == true:
@@ -135,9 +175,3 @@ func spell_hit(attack):
 	receive_damage(attack)
 	
 	
-##this is a signal sent to us from the hurtbox
-##When a area checked the hitbox layer enters it, and it is not our own.
-#func _on_Hurtbox_area_entered(area):
-#	if area.owner != self:
-#		print("ouch bro!")
-
