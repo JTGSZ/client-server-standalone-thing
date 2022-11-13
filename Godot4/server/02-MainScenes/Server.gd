ea0ec6bba2a748bc621ffb4d6b09a58a83e3b01f
JTGSZ <46565256+JTGSZ@users.noreply.github.com>
2022-11-13 03:11:50 -0500

    uhhhhhhhhh

--- Godot4/server/02-MainScenes/Server.gd
+++ Godot4/server/02-MainScenes/Server.gd
@@ -1,27 +1,12 @@
-extends Node
+extends Control
+var multiplayer_peer = ENetMultiplayerPeer.new()
 
-#make new node lol
-var network = ENetMultiplayerPeer.new()
-# port number obv what this be
-var port = 1909
-#req field ironically
-var max_players = 100
 
-func _ready():
-	StartServer();
-	
-func StartServer():
-	network.create_server(port, max_players)
-	get_tree().set_multiplayer_peer(network)
-	print("Server started")
-	
-	#Attach some signals to the network node
-	#these ones fire unchecked when we have a peer connect or disconnect
-	network.connect("peer_connected",Callable(self,"_Peer_Connected"))
-	network.connect("peer_disconnected",Callable(self,"_Peer_Disconnected"))
 
-func _Peer_Connected(player_id):
-	print("User " + str(player_id) + " Connected")
-	
-func _Peer_Disconnected(player_id):
-	print("User " + str(player_id) + " Disconnected")
+func _on_button_pressed():
+	multiplayer_peer.create_server(
+		$Menu/PortInput.text.to_int(),
+		$Menu/MaxPlayerInput.text.to_int()
+	)
+	multiplayer.multiplayer_peer = multiplayer_peer
+	$Menu.visible = false
